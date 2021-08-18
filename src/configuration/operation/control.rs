1e4b30b628786186c55a7c452a854e14c49f19ed
Alejandro Martinez Ruiz <alex@flawedcode.org>
2021-08-18 16:17:07 +0200

    operation/control: add Cloned + others, change Assert/Refute
    Added Cloned from Stack, plus Group, Partial, and Pipe, plus changed the
    semantics of Assert and Refute to run a clone of the stack over all the
    ops discarding results.

--- src/configuration/operation/control.rs
+++ src/configuration/operation/control.rs
@@ -14,6 +14,19 @@ pub enum ControlError {
     InnerOperationError(#[from] Box<super::OperationError>),
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum StackExtendMode {
+    Prepend,
+    Append,
+}
+
+impl Default for StackExtendMode {
+    fn default() -> Self {
+        Self::Append
+    }
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum Control {
@@ -23,8 +36,22 @@ pub enum Control {
     OneOf(Vec<super::Operation>),
     All(Vec<super::Operation>),
     None(Vec<super::Operation>),
-    Assert(Box<super::Operation>),
-    Refute(Box<super::Operation>),
+    Assert(Vec<super::Operation>),
+    Refute(Vec<super::Operation>),
+    Group(Vec<super::Operation>),
+    Cloned {
+        #[serde(default)]
+        result: StackExtendMode,
+        ops: Vec<super::Operation>,
+    },
+    Partial {
+        #[serde(default)]
+        result: StackExtendMode,
+        ops: Vec<super::Operation>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        max: Option<usize>,
+    },
+    Pipe(Vec<super::Operation>),
     Log {
         #[serde(default)]
         level: LogLevel,
@@ -91,20 +118,73 @@ impl Control {
                     stack
                 })
                 .ok_or(ControlError::RequirementNotSatisfied)?,
-            Self::Assert(op) => super::process_operations(vec![input.clone()], &[op])
-                .is_ok()
-                .then(|| {
-                    stack.push(input);
-                    stack
-                })
-                .ok_or(ControlError::RequirementNotSatisfied)?,
-            Self::Refute(op) => super::process_operations(vec![input.clone()], &[op])
-                .is_err()
-                .then(|| {
-                    stack.push(input);
-                    stack
-                })
-                .ok_or(ControlError::RequirementNotSatisfied)?,
+            Self::Assert(ops) => {
+                stack.push(input);
+
+                let _ = super::process_operations(stack.clone(), ops)
+                    .map_err(|_| ControlError::RequirementNotSatisfied)?;
+
+                stack
+            }
+            Self::Refute(ops) => {
+                stack.push(input);
+
+                if super::process_operations(stack.clone(), ops).is_ok() {
+                    return Err(ControlError::RequirementNotSatisfied);
+                }
+
+                stack
+            }
+            Self::Group(ops) => {
+                stack.push(input);
+                super::process_operations(stack, ops.as_slice())
+                    .map_err(|e| ControlError::InnerOperationError(e.into()))?
+            }
+            Self::Cloned { result, ops } => {
+                let new_stack = stack.clone();
+                match super::process_operations(new_stack, ops.as_slice()) {
+                    Ok(mut v) => match result {
+                        StackExtendMode::Append => {
+                            stack.extend(v.into_iter());
+                            stack
+                        }
+                        StackExtendMode::Prepend => {
+                            v.extend(stack.into_iter());
+                            v
+                        }
+                    },
+                    Err(e) => return Err(ControlError::InnerOperationError(Box::new(e))),
+                }
+            }
+            Self::Partial { result, ops, max } => {
+                let max = core::cmp::min(max.unwrap_or(1), 1);
+                let partial = stack.split_off(stack.len().saturating_sub(max));
+                if partial.is_empty() {
+                    return Err(ControlError::NoValuesError);
+                }
+
+                match super::process_operations(partial, ops.as_slice()) {
+                    Ok(mut v) => match result {
+                        StackExtendMode::Append => {
+                            stack.extend(v.into_iter());
+                            stack
+                        }
+                        StackExtendMode::Prepend => {
+                            v.extend(stack.into_iter());
+                            v
+                        }
+                    },
+                    Err(e) => return Err(ControlError::InnerOperationError(Box::new(e))),
+                }
+            }
+            Self::Pipe(ops) => {
+                stack.extend(
+                    super::process_operations(vec![input], ops.as_slice())
+                        .map_err(|_| ControlError::RequirementNotSatisfied)?
+                        .into_iter(),
+                );
+                stack
+            }
             Self::Log { level, msg } => {
                 crate::log!(&"[3scale-auth/config]", *level, "{}", msg);
                 stack.push(input);
