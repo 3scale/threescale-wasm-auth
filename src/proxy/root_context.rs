f2a9968cb34a6a96e75c833c982f3006ff156daa
Alejandro Martinez Ruiz <alex@flawedcode.org>
2021-08-31 18:23:34 +0200

    root_context: program the timer according to the TTL

--- src/proxy/root_context.rs
+++ src/proxy/root_context.rs
@@ -1,12 +1,18 @@
 use proxy_wasm::traits::{Context, RootContext};
 use proxy_wasm::types::{BufferType, ChildContext};
 
+use core::time::Duration;
+use std::time::SystemTime;
+
 use crate::configuration::Configuration;
 use crate::log::IdentLogger;
 use crate::util::rand::thread_rng::{thread_rng_init_fallible, ThreadRng};
 use crate::util::serde::ErrorLocation;
 
+use super::config_fetcher::FetcherState;
 use super::http_context::HttpAuthThreescale;
+
+const MIN_SYNC: u64 = 20;
 
 pub(super) struct RootAuthThreescale {
     vm_configuration: Option<Vec<u8>>,
@@ -15,6 +21,8 @@ pub(super) struct RootAuthThreescale {
     context_id: u32,
     id: u32,
     log_id: String,
+    fetcher: FetcherState,
+    config_deadline: SystemTime,
 }
 
 impl RootAuthThreescale {
@@ -26,6 +34,8 @@ impl RootAuthThreescale {
             context_id: 0,
             id: 0,
             log_id: String::new(),
+            fetcher: FetcherState::Inactive,
+            config_deadline: std::time::UNIX_EPOCH,
         }
     }
 }
@@ -83,10 +93,87 @@ impl Context for RootAuthThreescale {
 
         info!(self, "registered");
     }
+
+    fn on_http_call_response(
+        &mut self,
+        token_id: u32,
+        _num_headers: usize,
+        _body_size: usize,
+        _num_trailers: usize,
+    ) {
+        match &self.fetcher {
+            FetcherState::Inactive => {
+                // This could be due to receiving a new configuration mid-flight of a system request
+                // We'll just ignore the response and start over again, as system and/or config
+                // could have changed.
+                debug!(
+                    self,
+                    "ignoring call response due to configuration fetcher being inactive"
+                );
+            }
+            FetcherState::RulesFetched => {
+                warn!(self, "rules were already fetched but got a response !?");
+            }
+            FetcherState::FetchingRules(call_id, svc_id) => {
+                if *call_id != token_id {
+                    warn!(self, "seen a call response without the right token id");
+                }
+                debug!(self, "received response for rules for service {}", svc_id);
+                match self.get_http_call_response_body(0, usize::MAX) {
+                    Some(body) => {
+                        info!(self, "got list of services!");
+                        //let svclist = straitjacket::api::v0::service::LIST;
+                        let rulelist = straitjacket::api::v0::service::proxy::mapping_rules::LIST;
+                        let body_s = String::from_utf8_lossy(body.as_slice());
+                        //let res = svclist.parse_str(body_s.as_ref());
+                        let res = rulelist.parse_str(body_s.as_ref());
+                        match res {
+                            Ok(rules) => {
+                                info!(self, "rules: {:#?}", rules);
+                                self.fetcher = FetcherState::RulesFetched;
+                            }
+                            Err(e) => {
+                                error!(self, "failed to parse list of rules: {}", e);
+                                match serde_json::from_str::<serde_json::Value>(body_s.as_ref())
+                                    .and_then(|json_val| {
+                                        serde_json::to_string_pretty(&json_val)
+                                            .or_else(|_| serde_json::to_string(&json_val))
+                                    }) {
+                                    Ok(json) => error!(self, "JSON error response:\n{}", json),
+                                    Err(_) => {
+                                        error!(self, "RAW error response:\n{}", body_s.as_ref())
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    None => {
+                        info!(self, "FAILED TO GET list of mapping rules!");
+                    }
+                }
+            }
+            FetcherState::FetchingConfigs(call_id) => {
+                if *call_id != token_id {
+                    warn!(self, "seen a call response without the right token id");
+                }
+            }
+            FetcherState::Error(_) => todo!(),
+        }
+    }
 }
 
 impl RootContext for RootAuthThreescale {
     fn on_vm_start(&mut self, vm_configuration_size: usize) -> bool {
+        info!(
+            self,
+            "{}",
+            concat!(
+                env!("CARGO_PKG_NAME"),
+                " version ",
+                env!("CARGO_PKG_VERSION"),
+                " booting up."
+            )
+        );
         info!(
             self,
             "on_vm_start: vm_configuration_size is {}", vm_configuration_size
@@ -170,6 +257,11 @@ impl RootContext for RootAuthThreescale {
             "on_configure: plugin configuration {:#?}", self.configuration
         );
 
+        // cancel any previous work updating configurations
+        self.fetcher = FetcherState::Inactive;
+
+        let _ = self.set_next_tick();
+
         true
     }
 
@@ -183,5 +275,131 @@ impl RootContext for RootAuthThreescale {
         };
 
         Some(ChildContext::HttpContext(Box::new(ctx)))
+    }
+
+    fn on_tick(&mut self) {
+        debug!(self, "executing on_tick");
+        if let Some(config) = self.get_configuration() {
+            if let Some(sys) = self.get_system_config() {
+                let current_time = self.get_current_time();
+                if current_time < self.config_deadline {
+                    warn!(
+                        self,
+                        "on_tick running while the configuration is still valid"
+                    );
+                }
+
+                let upstream = sys.upstream();
+                let new_state = match &self.fetcher {
+                    FetcherState::Inactive => {
+                        if let Some(services) = config.services() {
+                            info!(self, "fetching rules for {} services", services.len());
+                            let ruleslist =
+                                straitjacket::api::v0::service::proxy::mapping_rules::LIST;
+                            let method = ruleslist.method().as_str();
+                            let qs = format!("access_token={}", sys.token());
+                            let results = services.iter().map(|svc| {
+                                let svc_id = svc.id();
+                                let path = ruleslist.path(&[svc_id]);
+                                if let Ok(path) = path {
+                                    match upstream.call(
+                                        self,
+                                        path.as_str(),
+                                        method,
+                                        vec![],
+                                        Some(qs.as_str()),
+                                        None,
+                                        None,
+                                        None,
+                                    ) {
+                                        Ok(call_id) => {
+                                            debug!(self, "fetching rules list for service {}", svc_id);
+                                            FetcherState::FetchingRules(call_id, svc_id.to_string())
+                                        }
+                                        Err(e) => {
+                                            error!(
+                                                self,
+                                                "failed to initiate fetch of mapping rules list for service {}: {}", svc_id, e
+                                            );
+                                            FetcherState::Error(e.into())
+                                        }
+                                    }
+                                } else {
+                                    critical!(
+                                        self,
+                                        "failed to obtain path for mapping rules list API: {}",
+                                        path.unwrap_err()
+                                    );
+                                    FetcherState::Inactive
+                                }
+                            });
+                            FetcherState::Inactive
+                        } else {
+                            FetcherState::Inactive
+                        }
+                    }
+                    FetcherState::FetchingRules(token_id, svc_id) => {
+                        info!(
+                            self,
+                            "still fetching rules!? - token_id: {}, svc_id: {}", token_id, svc_id
+                        );
+                        unimplemented!()
+                    }
+                    FetcherState::RulesFetched => {
+                        info!(self, "fetched rules");
+                        FetcherState::Inactive
+                    }
+                    FetcherState::FetchingConfigs(token_id) => {
+                        info!(self, "still fetching configs!? - token_id: {}", token_id);
+                        unimplemented!()
+                    }
+                    FetcherState::Error(_) => todo!(),
+                };
+
+                self.fetcher = new_state;
+                self.set_next_tick();
+            }
+        }
+    }
+}
+
+impl RootAuthThreescale {
+    pub fn get_configuration(&self) -> Option<&crate::configuration::api::v1::Configuration> {
+        self.configuration.as_ref().map(|conf| conf.get())
+    }
+
+    pub fn get_system_config(&self) -> Option<&crate::threescale::System> {
+        self.get_configuration().map(|conf| conf.system()).flatten()
+    }
+
+    fn get_next_tick(&self) -> Option<(Duration, Duration)> {
+        self.get_system_config().map(|sys| {
+            let jitter = self.rng.next_u32() as u64 & 0x0F; // add 0-15 seconds on top
+
+            // ensure we only do this at most once per minute, and at least not within the timeout
+            let original_ttl = core::cmp::min(
+                sys.ttl(),
+                core::cmp::max(Duration::from_secs(MIN_SYNC), sys.upstream().timeout),
+            );
+            let ttl = original_ttl.saturating_add(Duration::from_secs(jitter));
+            info!(
+                self,
+                "system configuration TTL set to {} seconds",
+                ttl.as_secs()
+            );
+
+            (ttl, original_ttl)
+        })
+    }
+
+    pub fn set_next_tick(&mut self) -> Option<Duration> {
+        self.get_next_tick().map(|(tick, original_ttl)| {
+            self.config_deadline = self
+                .get_current_time()
+                .checked_add(original_ttl)
+                .unwrap_or(std::time::UNIX_EPOCH);
+            self.set_tick_period(tick);
+            tick
+        })
     }
 }
