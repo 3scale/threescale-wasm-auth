8a16a66735ba87a81078beb56b175ee4ca2893ac
Alejandro Martinez Ruiz <alex@flawedcode.org>
2021-10-11 20:31:55 +0530

    config_fetcher/thread_local: add getter and initializer for thread local

--- src/proxy/config_fetcher/thread_local.rs
+++ src/proxy/config_fetcher/thread_local.rs
@@ -1,12 +1,40 @@
 use super::*;
 
-pub use imp::fetcher_with;
+// Thread RNG callable from anywhere within the thread.
+// Safe to call this from different contexts as well and
+// obtain ThreadRng instances, since they will all use
+// the same thread local pseudo RNG.
+#[inline]
+#[allow(dead_code)]
+pub fn fetcher_init_fallible() -> Result<Fetcher, Error> {
+    Fetcher::get()
+}
+
+// Thread RNG callable from anywhere within the thread.
+// Safe to call this from different contexts as well and
+// obtain ThreadRng instances, since they will all use
+// the same thread local pseudo RNG.
+//
+// Panic: will panic if the thread local RNG could not be initialized.
+#[inline]
+#[allow(dead_code)]
+pub fn fetcher_init() -> Fetcher {
+    fetcher_init_fallible().expect("could not initialize thread local configuration fetcher")
+}
 
 pub struct Fetcher;
 
 impl Fetcher {
     pub fn get() -> Result<Self, Error> {
         imp::initialize().and(Ok(Self))
+    }
+
+    pub fn with<R, F: FnOnce(&mut Vec<ConfigFetcher>) -> R>(f: F) -> R {
+        imp::fetcher_with(f)
+    }
+
+    pub fn clear() {
+        imp::fetcher_with(|vcf| vcf.clear());
     }
 }
 
@@ -35,7 +63,7 @@ mod imp {
         })
     }
 
-    pub fn fetcher_with<R, F: FnOnce(&mut Vec<ConfigFetcher>) -> R>(f: F) -> R {
-        FETCHER.with(|rng| f(rng.borrow_mut().as_mut().unwrap()))
+    pub(super) fn fetcher_with<R, F: FnOnce(&mut Vec<ConfigFetcher>) -> R>(f: F) -> R {
+        FETCHER.with(|vcf| f(vcf.borrow_mut().as_mut().unwrap()))
     }
 }
